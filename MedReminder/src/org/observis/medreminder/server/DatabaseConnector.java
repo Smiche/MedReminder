594cad963101882fb313594698b8e642aa3590fb
idumpweed <idumpweed@hotmail.com>
2016-05-03 13:22:37 +0200

    database class

--- MedReminder/src/org/observis/medreminder/server/DatabaseConnector.java
+++ MedReminder/src/org/observis/medreminder/server/DatabaseConnector.java
@@ -4,10 +4,40 @@ import	java.sql.*;
 
 
 public class DatabaseConnector {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver"; 
+	static final String DB_URL = "jdbc:mysql://192.168.8.110//patiens";
+	//url to database
+	 //  Database credentials
+	static final String USER = "root";
+	static final String PASS = "rootroot";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Connection conn = null;
+		try {
+			//driver registration 
+			Class.forName("com.mysql.jdbc.Driver");
+			//opening a connection
+			 System.out.println("Connecting to a selected database...");
+		     conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		     System.out.println("Connected database successfully...");
+		} catch(SQLException se){
+			//Handle errors for JDBC
+		      se.printStackTrace();
+		} catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   } finally{
+			   try{
+				   if(conn!=null)
+					   conn.close();
+			   
+			   }catch(SQLException se){
+				   se.printStackTrace();
+			   }//end finally try
+		   }//end try
+		System.out.println("it's done");
+		
 
-	}
+	}//end main
 
-}
+}//end
