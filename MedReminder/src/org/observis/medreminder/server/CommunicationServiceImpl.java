0924405e5653f6b4a07e97b93c064cb142d44b14
Smiche <smichess@gmail.com>
2016-05-04 15:49:29 +0300

    serial

--- MedReminder/src/org/observis/medreminder/server/CommunicationServiceImpl.java
+++ MedReminder/src/org/observis/medreminder/server/CommunicationServiceImpl.java
@@ -9,6 +9,11 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class CommunicationServiceImpl extends RemoteServiceServlet implements CommunicationService{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public String addPatient(String name, String phone)
 			throws IllegalArgumentException {
