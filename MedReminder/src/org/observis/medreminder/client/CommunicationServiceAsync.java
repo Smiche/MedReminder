bede5af57667bb3a1d2142083b7196b244cc1962
Smiche <smichess@gmail.com>
2016-05-04 15:32:46 +0300

    template stuff

--- MedReminder/src/org/observis/medreminder/client/CommunicationServiceAsync.java
+++ MedReminder/src/org/observis/medreminder/client/CommunicationServiceAsync.java
@@ -4,7 +4,9 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface CommunicationServiceAsync {
 	void addPatient(String input, String phone, AsyncCallback<String> callback) throws IllegalArgumentException;
-	void addTemplate(String name,String day,String time, AsyncCallback<Void> callback)throws IllegalArgumentException;
+	void addTemplate(String text,String weekDays,String time,String duration, AsyncCallback<Void> callback)throws IllegalArgumentException;
+	void getTemplate(String text,String weekDays,String time,String duration, AsyncCallback<String[]> callback)throws IllegalArgumentException;
+	void getTemplateList(AsyncCallback<String> callback) throws IllegalArgumentException;
 	void getPatients(AsyncCallback<String> callback)throws IllegalArgumentException;
 	
 }
