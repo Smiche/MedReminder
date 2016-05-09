b05ecba15382158938a3a6fc4eac7293d7372113
Smiche <smichess@gmail.com>
2016-05-09 10:54:03 +0300

    bugs

--- MedReminder/src/org/observis/medreminder/client/Message.java
+++ MedReminder/src/org/observis/medreminder/client/Message.java
@@ -1,18 +1,23 @@
 package org.observis.medreminder.client;
 
-import java.util.ArrayList;
+import java.io.Serializable;
 
-public class Message {
-	public String title;
-	public String text;
-	public String time;
-	public String day;
+public class Message implements Serializable{
+	private static final long serialVersionUID = -5828108890651522661L;
+	public String title = "";
+	public String text = "";
+	public String time = "";
+	public String day = "";
 	
 	public Message(String title, String text, String time, String day){
 		this.title = title;
 		this.text = text;
 		this.time = time;
 		this.day = day;		
+	}
+	
+	public Message(){
+		
 	}
 	
 }
