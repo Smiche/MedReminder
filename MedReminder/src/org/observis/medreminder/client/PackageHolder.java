e0ec6aca030150bca6075e68505b9f2c6e027576
Smiche <smichess@gmail.com>
2016-05-11 10:16:39 +0300

    new favicon

--- MedReminder/src/org/observis/medreminder/client/PackageHolder.java
+++ MedReminder/src/org/observis/medreminder/client/PackageHolder.java
@@ -2,6 +2,7 @@ package org.observis.medreminder.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
@@ -16,6 +17,8 @@ public class PackageHolder extends HorizontalPanel{
 	private TextBox messageHourBox = new TextBox();
 	private TextBox messageMinuteBox = new TextBox();
 	private TextBox messageDayBox = new TextBox();
+	
+	private DialogBox createMessageBox = new DialogBox();
 	
 	
 }
